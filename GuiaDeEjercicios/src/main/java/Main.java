73209bc29f57c970dc011a64453cbe309c5c81c1
Alvaro Lopez <lt0895032020@unab.edu.sv>
2022-02-04 20:02:22 -0600

    Ejercicio 4

--- GuiaDeEjercicios/src/main/java/Main.java
+++ GuiaDeEjercicios/src/main/java/Main.java
@@ -1,59 +1,95 @@
 
-import java.util.Scanner;
+//import java.util.Scanner;
 /**
  *
  * @author Alvaro 
  */
-public class Main {
+//public class Main {
+////////////Ejercicio 4//////////////////
+//    public static void main(String []args)
+//{
+//     int i = 8;
+//     int j = 5;
+//     float x = 0.005F;
+//     float y = -0.01F;
+//     char c = 'c';
+//     char d = 'd';
+//        
+//
+//        System.out.println(i<=j);
+//        System.out.println(c > d);
+//        System.out.println(x >= 0);
+//        System.out.println(x < y--);
+//        System.out.println(j != 6);
+//        System.out.println(c == 99);
+//        System.out.println(-(i+j)!=-i+j);
+//        System.out.println(i <= j &&i >= c);    
+//}     
 ///////////Ejercicio 5/////////////////
-     Scanner Datos = new Scanner(System.in);    
-     
-     int X, Y ;
-     double M, N;
-     
-     public void Datos()
-     {
-     System.out.println("Ingrese el dato X");
-     X = Datos.nextInt();
-     System.out.println("Ingrese el dato Y");
-     Y = Datos.nextInt();
-     System.out.println("Ingrese el dato M");
-     M = Datos.nextDouble();
-     System.out.println("Ingrese el dato N");
-     N = Datos.nextDouble();
-     }
-     
-     public void MDatos()
-     {
-     System.out.println("Suma X + Y: " + X + Y);
-     System.out.println(X - Y + ":Diferencia de X - Y");
-     System.out.println("Multiplicacion X * Y: " + X * Y);
-     System.out.println("Cociente X / Y: " + X / Y);
-     System.out.println("Resto X % Y: " + X % Y);
-     
-     System.out.println("Suma N + M: " + N + M);
-     System.out.println(N - M + " :Diferencia N - M ");
-     System.out.println("Multiplicacion N * M: " + N * M);
-     System.out.println("Cociente N / M: " +  N / M);
-     System.out.println("Resto N % M: " + N % M);
-     
-     System.out.println("Suma de X + N: " +  X + N);
-     System.out.println("Cociente de Y / M: " + Y / M);
-     System.out.println("Resto de Y % M: " + Y % M);
-     
-     System.out.println("2 veces X: " + 2*X);
-     System.out.println("2 veces Y: " + 2*Y);
-     System.out.println("2 veces N: " + 2*N);
-     System.out.println("2 veces M: " + 2*M);
-     
-     System.out.println("Suma de X+Y+M+N: " + X+Y+N+M);
-     System.out.println("Multiplicacion de X*Y*M*N: " + X*Y*N*M);
-     }
-     
-     public static void main(String [] args)
-     {
-         Main d = new Main();
-         d.Datos();
-        d.MDatos();
-     }
+//     Scanner Datos = new Scanner(System.in);    
+//     
+//     int X, Y ;
+//     double M, N;
+//     
+//     public void Datos()
+//     {
+//     System.out.println("Ingrese el dato X");
+//     X = Datos.nextInt();
+//     System.out.println("Ingrese el dato Y");
+//     Y = Datos.nextInt();
+//     System.out.println("Ingrese el dato M");
+//     M = Datos.nextDouble();
+//     System.out.println("Ingrese el dato N");
+//     N = Datos.nextDouble();
+//     }
+//     
+//     public void MDatos()
+//     {
+//     System.out.println("Suma X + Y: " + X + Y);
+//     System.out.println(X - Y + ":Diferencia de X - Y");
+//     System.out.println("Multiplicacion X * Y: " + X * Y);
+//     System.out.println("Cociente X / Y: " + X / Y);
+//     System.out.println("Resto X % Y: " + X % Y);
+//     
+//     System.out.println("Suma N + M: " + N + M);
+//     System.out.println(N - M + " :Diferencia N - M ");
+//     System.out.println("Multiplicacion N * M: " + N * M);
+//     System.out.println("Cociente N / M: " +  N / M);
+//     System.out.println("Resto N % M: " + N % M);
+//     
+//     System.out.println("Suma de X + N: " +  X + N);
+//     System.out.println("Cociente de Y / M: " + Y / M);
+//     System.out.println("Resto de Y % M: " + Y % M);
+//     
+//     System.out.println("2 veces X: " + 2*X);
+//     System.out.println("2 veces Y: " + 2*Y);
+//     System.out.println("2 veces N: " + 2*N);
+//     System.out.println("2 veces M: " + 2*M);
+//     
+//     System.out.println("Suma de X+Y+M+N: " + X+Y+N+M);
+//     System.out.println("Multiplicacion de X*Y*M*N: " + X*Y*N*M);
+//     }
+//     
+//     public static void main(String [] args)
+//     {
+//         Main d = new Main();
+//         d.Datos();
+//        d.MDatos();
+//     }
+/////////////////////////////////////////////////////////    
+    
+    
+/////////////Ejercicio 6 ////////////////////////
+
+//public static void main(String []args)
+//{
+//   int A = 1;
+//   int B = 2;
+//   int C = 3;
+//   int D = 4;
+//   
+//   
+//
+//    
+//}    
 }
